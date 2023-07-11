4c2bd8d0c39c852c77a47e986f66dbc6342b1b30
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-07-11 16:37:01 -0400

    added display function

--- calculator.js
+++ calculator.js
@@ -37,10 +37,9 @@ number.forEach((button) => {
 
     button.addEventListener('click', () => {
 let value = button.value
-console.log(value)
 
-const display = document.querySelector('#display-container');
-display.textContent = value;
+display(value)
+
     })
 })
 
@@ -51,6 +50,16 @@ operator.forEach((button) => {
     button.addEventListener('click', () => {
 let value = button.value
 console.log(value)
+/*not working --------
+const display2 = document.querySelector('#display-container2');
+display2.textContent = value;*/
 
     })
 })
+let numArr = []
+function display (number) {
+numArr.push(number) 
+
+    const display = document.querySelector('#display-container');
+display.textContent = numArr.join('');
+}
